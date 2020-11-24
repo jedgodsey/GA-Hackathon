79b8b47e30b152f21cad20cbf7f19b4389f21f0a
jedgodsey <jed.godsey@gmail.com>
2020-11-23 17:04:06 -0800

    pending array still has issues

--- GA-Hackathon/my-app/src/components/Scoreboard.js
+++ GA-Hackathon/my-app/src/components/Scoreboard.js
@@ -8,6 +8,7 @@ class Scoreboard extends React.Component {
       <div>
         <h1>Scorard</h1>
         <h2>Score: {this.props.score}</h2>
+        <h2>Pending tasks: {this.props.pending}</h2>
       </div>
     )
   }
--- GA-Hackathon/my-app/src/components/SlotMachine.js
+++ GA-Hackathon/my-app/src/components/SlotMachine.js
@@ -5,7 +5,8 @@ class SlotMachine extends React.Component {
   state = {
     score: 0,
     options: ['option 1', 'option 2', 'option 3', 'option 4'],
-    task: ''
+    task: '',
+    pending: []
   }
 
   onTaskComplete = () => {
@@ -14,9 +15,14 @@ class SlotMachine extends React.Component {
   
   selector = () => {
     let selection = this.state.options[Math.floor(Math.random() * this.state.options.length)]
+    console.log('selection: ', selection)
+    console.log('state pending', this.state.pending)
+    let newPending = this.state.pending.push(selection);
+    // let newOptions = this.state.options.filter(item => item !== selection)
     this.setState({
       task: selection,
-      options: this.state.options.filter(item => item !== selection)
+      // options: newOptions,
+      pending: newPending
     })
     return selection;
   }
@@ -24,8 +30,18 @@ class SlotMachine extends React.Component {
   render() {
     return(
       <>
-        <Scoreboard score={this.state.score} />
+        <Scoreboard score={this.state.score} pending={this.state.pending} />
         <h1>Slot Machine</h1>
+        <div className="dropdown">
+          <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown button
+          </button>
+          <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a className="dropdown-item" href="#">Action</a>
+            <a className="dropdown-item" href="#">Another action</a>
+            <a className="dropdown-item" href="#">Something else here</a>
+          </div>
+        </div>
         {this.state.task}
         <button onClick={this.selector}>give me a task</button>
         <button onClick={this.onTaskComplete}>Did it!</button>
