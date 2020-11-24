b667d00f2c86406a5e554c237b38e59fc330bc87
jedgodsey <jed.godsey@gmail.com>
2020-11-23 22:18:47 -0800

    committing before test

--- GA-Hackathon/my-app/src/components/Navbar.js
+++ GA-Hackathon/my-app/src/components/Navbar.js
@@ -3,10 +3,24 @@ import React from 'react';
 class Navbar extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">Best App Ever</a>
-        <a className="navbar-brand" href="#" onClick={this.props.toggleView} >Logout</a>
-      </nav>
+      // <nav className="navbar navbar-dark bg-dark nav nav-tabs">
+        /* <a className="navbar-brand" href="#">Best App Ever</a>
+        <a className="navbar-brand" href="#" onClick={this.props.toggleView} >Logout</a> */
+        <ul className="nav nav-tabs ml-auto">
+          <li className="nav-item">
+            <a className="nav-link active" href="#">Active</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+      // </nav>
     )
   }
 }
