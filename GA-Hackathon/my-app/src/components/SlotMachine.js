7b0a09c77c2dffe81841529c970a6c8d29986a09
morganjaouni <morgan.jaouni@ttu.edu>
2020-11-24 13:17:07 -0600

    styles

--- GA-Hackathon/my-app/src/components/SlotMachine.js
+++ GA-Hackathon/my-app/src/components/SlotMachine.js
@@ -11,9 +11,9 @@ class SlotMachine extends React.Component {
       <>
         <div className="row ">
           <div className="col-6 content-left">
-            <div className="d-flex taskDir">
-            <p>find an activity</p>
-              <div className="dropdown">
+            <div className="d-flex taskDir p-3">
+              <div className="dropdown p-3">
+            <p class="p-3"> find an activity</p>
                 <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   suprise me!
                 </button>
@@ -26,17 +26,17 @@ class SlotMachine extends React.Component {
                 </div>
               </div>
               <div>
-                <img src={go} onClick={this.props.selector}></img>
+                <img src={go} class="p-3" onClick={this.props.selector}></img>
               </div>
             </div>
           </div>
           <div className="col-6 content-right">
             <div className="d-flex taskPopUp">
-                <div className="taskDir">
+                <div className="taskDir h1" id="task">
                   {this.props.task}
                 </div>
                 <div className="did-it">
-                  {this.props.options.length > 0 ? <button className="btn btn-primary" onClick={this.props.onTaskComplete}>Did it!</button> : null}
+                  {this.props.options.length > 0 ? <button className="did-btn" onClick={this.props.onTaskComplete}>Did it!</button> : null}
                 </div>
             </div>
           </div>
