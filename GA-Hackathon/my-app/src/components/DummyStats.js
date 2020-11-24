c9fa49f6d3242af86d0fa595716b7fe91f0fec2c
jedgodsey <jed.godsey@gmail.com>
2020-11-24 10:58:03 -0800

    finished tab functionality for bio pages

--- GA-Hackathon/my-app/src/components/DummyStats.js
+++ GA-Hackathon/my-app/src/components/DummyStats.js
@@ -1,9 +1,18 @@
-import statsPic from "../images/02_1_1Stats_Card.svg"
+import statsPic from "../images/02_1_1Stats_Card.svg";
+import statsIcon from "../images/Stats_Icon.svg";
+import profileIcon from "../images/Profile_Icon.svg";
 
 const DummyStats = (props) => {
   return(
     <>
-      <button onClick={props.toggleDummy}>Show Profile</button>
+      <div className="tab-container">
+        <div className="tab dark-tab">
+          <img src={statsIcon} className="tab-icon" />
+        </div>
+        <div className="tab light-tab" onClick={props.toggleDummy}>
+          <img src={profileIcon} className="tab-icon" />
+        </div>
+      </div>
       <div className="dummy-background">
         <img src={statsPic} />
       </div>
